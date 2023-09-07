ea1b3f540695ad2168eba61a3212d828f0153759
Yissel-Cuellar <nancy.cuellar@soytul.com>
2023-09-07 14:36:20 -0600

    init reset

--- src/test/kotlin/com/example/msproduct/TestData.kt
+++ src/test/kotlin/com/example/msproduct/TestData.kt
@@ -2,6 +2,7 @@ package com.example.msproduct
 
 import com.example.msproduct.dto.ProductDto
 import com.example.msproduct.entity.ProductEntity
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
 
 open class TestData {
     fun msProduct() : ProductEntity {
@@ -11,6 +12,27 @@ open class TestData {
             price = 35.76
             description = "description"
         }
+    }
+    fun msProductList() : List<ProductEntity> {
+        return listOf(
+            ProductEntity().apply{
+                    id = 1
+                    name = "Jabon"
+                    price = 35.76
+                    description = "description"
+            },
+            ProductEntity().apply{
+                    id = 1
+                    name = "Jabon"
+                    price = 35.76
+                    description = "description"
+            },
+            ProductEntity().apply{
+                    id = 1
+                    name = "Jabon"
+                    price = 35.76
+                    description = "description"
+            })
     }
 
 }
