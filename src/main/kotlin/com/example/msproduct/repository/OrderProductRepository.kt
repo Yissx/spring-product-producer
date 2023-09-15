35b819c8d9e3a3e52f3943cd9778f2cf61c9639e
Yissx <yisselcuellar.yc401@gmail.com>
2023-09-15 13:40:04 -0600

    ID -> UUID

--- src/main/kotlin/com/example/msproduct/repository/OrderProductRepository.kt
+++ src/main/kotlin/com/example/msproduct/repository/OrderProductRepository.kt
@@ -3,8 +3,9 @@ package com.example.msproduct.repository
 import com.example.msproduct.entity.OrderProductEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
-interface OrderProductRepository : JpaRepository<OrderProductEntity, Long>{
+interface OrderProductRepository : JpaRepository<OrderProductEntity, UUID>{
 
 }
