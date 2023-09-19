d196c243a99f2a0cf2dfb397652f5d4cbfb0b76f
Yissx <yisselcuellar.yc401@gmail.com>
2023-09-19 14:01:05 -0600

    .

--- src/main/kotlin/com/example/msproduct/mapper/OrderMapper.kt
+++ src/main/kotlin/com/example/msproduct/mapper/OrderMapper.kt
@@ -4,14 +4,8 @@ import com.example.msproduct.dto.OrderDto
 import com.example.msproduct.entity.ClientEntity
 import com.example.msproduct.entity.OrderEntity
 import com.example.msproduct.entity.ProductEntity
-import org.springframework.data.jpa.domain.AbstractPersistable_.id
 import org.springframework.stereotype.Service
 
-/*@Mapper(componentModel = "spring",
-    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-    unmappedTargetPolicy = ReportingPolicy.IGNORE,
-    nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
-)*/
 @Service
 class OrderMapper {
 
@@ -27,6 +21,7 @@ class OrderMapper {
             )
         }
     }
+
     fun toEntity(orderDto: OrderDto, productEntities : List<ProductEntity>, clientEntity : ClientEntity) : OrderEntity {
         return orderDto.let {
             OrderEntity(
